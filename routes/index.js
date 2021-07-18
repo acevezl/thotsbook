f48e6277c01b0f5357893019de2fd1459848b65b
Luis Arnaut <arnaut@Arnaut-MacBook-Pro.local>
2021-07-18 15:09:40 -0700

    added routes/index

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+// add prefix of /api to all the api routes imported
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+  res.status(404).send('<h1>😝 404 Error!</h1>');
+});
+
+module.exports = router;
