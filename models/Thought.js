eea12f95f5774a2741bdfb76dc1d41b92218aaad
Luis Arnaut <arnaut@Arnaut-MacBook-Pro.local>
2021-07-18 20:58:29 -0700

    added Thought model

---
+++ models/Thought.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: [ true, 'The thought text is mandatory' ],
+            min: 1,
+            max: [ 128, 'Thoughts must have a maximum of 128 characters' ]
+        },
+        createdAt: {
+            type: Date,
+            default: Default.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: [ true, 'Username is required']
+        },
+        reactions: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Reaction'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+ThoughtSchema.virtual('reactionCount').get(
+    function() {
+        return this.reactions.length;
+    }
+);
