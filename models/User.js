f3139f4360304e117c4728e80ff15145e1510441
Luis Arnaut <arnaut@Arnaut-MacBook-Pro.local>
2021-07-18 19:43:54 -0700

    created User model

---
+++ models/User.js
@@ -0,0 +1,52 @@
+const { Schema, model } = require ('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: [ true, 'This username is not available' ],
+            required: [ true, 'Username is required'],
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: [ true, 'This e-mail is already registered' ],
+            required: [ true, 'E-mail is required' ],
+            trim: true,
+            match: [/^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/]
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+UserSchema.virtual('friendCount').get(
+    function() {
+        return this.friends.length;
+    }
+);
+
+UserSchema.virtual('thoughtCount').get(
+    function() {
+        return this.thoughts.length;
+    }
+);
+
